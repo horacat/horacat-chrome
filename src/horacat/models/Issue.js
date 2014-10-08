9dcaf6dfd87baaa88c810c00da99d5bf4eca9595
Daniel Bugl <daniel.bugl@touchlay.com>
2014-10-08 18:49:20 +0200

    Issue model

--- src/horacat/models/Issue.js
+++ src/horacat/models/Issue.js
@@ -1,32 +1,123 @@
-/*
-{
-  "project_hash": {
-    "issue_hash": {
-      "active": {
-        "logged": 100,
-        "start": 1231283
-      },
-      "logs": [
-        { "user": "user_hash", "start": 1231283, "stop": 1231383 }
-      ]
-    }
-  }
-}
+/**
+ * Represents a (GitHub) issue
+ * 
+ * @class
+ * @param {object} config The data in the issue, see Issue.set() for more information
  */
+function Issue(config) {
+  this.logs = [];
 
-// start and logged are per-user
-function Timer(start, logged, logs) {
-  if (logged) this.logged = logged;
-  else this.logged = 0;
-  if (start) this.start = start;
-  else this.start = 0;
-  if (logs) this.logs = logs;
+  this.set(config);
 }
 
-Timer.prototype.addLog = function addLog(log) {
+/**
+ * Set active timer of the issue
+ * @param {Timer} timer Active Timer object
+ */
+Issue.prototype.setTimer = function setTimer(timer) {
+  if (!timer instanceof Timer) {
+    throw new Error('Invalid type of argument \'timer\'. Expected: Timer');
+  }
+
+  this.timer = timer;
+};
+
+/**
+ * Get active timer of the issue
+ * @param {boolean} raw Return raw timer data (e.g. for JSON exports)
+ * @return {Timer|object} Active Timer object (or data, if raw was specified)
+ */
+Issue.prototype.getTimer = function getTimer(raw) {
+  if (raw) {
+    return this.timer.get();
+  } else {
+    return this.timer;
+  }
+};
+
+/**
+ * Add log to the issue
+ * @param {Log} log Log object
+ */
+Issue.prototype.addLog = function addLog(log) {
   if (!log instanceof Log) {
-    throw new Exception("Invalid log.");
+    throw new Error('Invalid type of argument \'log\'. Expected: Log');
   }
 
   this.logs.push(log);
+};
+
+/**
+ * Set logs of the issue
+ * @param {array} logs Array of Log objects
+ */
+Issue.prototype.setLogs = function setLogs(logs) {
+  if (!logs instanceof Array) {
+    throw new Error('Invalid type of argument \'logs\'. Expected: Array');
+  }
+
+  for (var i=0; i < logs.length; i++) {
+    if (!logs[i] instanceof Log) {
+      throw new Error('Invalid type of argument \'logs[' + i + ']\'. Expected: Log');
+    }
+
+    this.addLog(logs[i]);
+  }
+};
+
+/**
+ * Get logs of the issue
+ * @param {boolean} raw Return raw log data (e.g. for JSON exports)
+ * @param {array} logs Array of Log objects (or array of log data, if raw was specified)
+ */
+Issue.prototype.getLogs = function getLogs(raw) {
+  if (raw) {
+    var rawLogs = [];
+    for (var i=0; i < this.logs.length; i++) {
+      rawLogs.push(this.logs[i].get());
+    }
+    return rawLogs;
+  } else {
+    return this.logs;
+  }
+};
+
+/**
+ * Get active timer of the issue
+ * @param {boolean} raw Return raw timer data (e.g. for JSON exports)
+ * @return {Timer|object} Active Timer object (or data, if raw was specified)
+ */
+Issue.prototype.getTimer = function getTimer(raw) {
+  if (raw) {
+    return this.timer.get();
+  } else {
+    return this.timer;
+  }
+};
+
+/**
+ * Set multiple values of the issue
+ *
+ * Values:
+ *  - timer: see setTimer()
+ *  - logs: see setLogs()
+ * 
+ * @param {object} config Configuration Object
+ */
+Issue.prototype.set = function set(config) {
+  if (config) {
+    if (config.timer) this.setTimer(config.timer);
+    if (config.logs) this.setLogs(config.logs);
+  }
+};
+
+/**
+ * Get data from the issue
+ * @return {object} issue data (e.g. for JSON exports)
+ */
+Issue.prototype.get = function get() {
+  return {
+    timer: this.getTimer(true),
+    logs: this.getLogs(true)
+  };
 };
